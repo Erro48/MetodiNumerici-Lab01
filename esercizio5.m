00477ad8e6980f0f23b42fabd5ad6da46e815547
Enrico Tagliaferri - enrico.tagliaferri2@studio.unibo.it <enrico.tagliaferri2@studio.unibo.it>
2022-02-24 09:53:17 +0100

    Draw function sin(kx) with k = 1/3, 1/2, 1, 2

--- esercizio5.m
+++ esercizio5.m
@@ -1,9 +1,14 @@
 clear all
 clc
 
-x = linspace(-pi, pi, 100);
-%y = zeros(1,100);
-y = sin(1/3 * x);
-
 figure(1)
-plot(x,y,'bo')
+hold on
+
+k = [1/3 1/2 1 2];
+color = ['b' 'r' 'g' 'k'];
+x = linspace(-pi, pi, 100);
+
+for i=1:1:4
+    y = sin(k(1,i) * x);
+    plot(x,y, [color(1,i), '-' ])
+end
