06f23e58b23d964cb2412deed50ca4e7919e1605
Enrico Tagliaferri - enrico.tagliaferri2@studio.unibo.it <enrico.tagliaferri2@studio.unibo.it>
2022-02-24 09:55:12 +0100

    Change vector color to colors

--- esercizio5.m
+++ esercizio5.m
@@ -5,10 +5,10 @@ figure(1)
 hold on
 
 k = [1/3 1/2 1 2];
-color = ['b' 'r' 'g' 'k'];
+colors = ['b' 'r' 'g' 'k'];
 x = linspace(-pi, pi, 100);
 
 for i=1:1:4
     y = sin(k(1,i) * x);
-    plot(x,y, [color(1,i), '-' ])
+    plot(x,y, [colors(1,i), '-' ])
 end
