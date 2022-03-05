1745585e5ac9b02ad6fdff4d41d579b2545f3d08
Enrico <errotagliaferri.4801@gmail.com>
2022-03-05 09:10:01 +0100

    Complete all points in exercise 2

--- es2.m
+++ es2.m
@@ -1,3 +1,20 @@
 clear all
 clc
 
+A = magic(4);
+B = [ 
+        0 1 0 0;
+        0 0 0 1;
+        1 0 0 0;
+        0 0 1 0
+    ];
+
+% a)
+B*A;
+A*B;
+
+% b) PA=A_mod
+A_mod = [A(3,:); A(2,:); A(1,:); A(4,:)];
+P = A_mod / A;
+P*A
+A*P
